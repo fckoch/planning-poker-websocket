1bd9ff284333f3c9dff045693b02748436ebb4d7
Filipe de Carvalho Koch <filipe.koch@gmail.com>
2022-04-05 22:43:50 -0300

    and select place

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
 import express from 'express';
 import { Server } from 'socket.io';
 import http from 'http';
+import { Player } from './player';
 
 const PORT = process.env.PORT || 8899;
 const app = express();
 const httpServer = http.createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*'
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
 io.on('connection', (socket) => {
+
     console.log('user connected');
+    let players: Player[] = [];
+
+    socket.on('select place', (player: Player) => {
+        console.log(players);
+        players = [
+            ...players,
+            player
+        ]
+        console.log(players);
+
+        socket.emit('select place', players);
+    })
 
     socket.on('disconnect', () => {
         console.log('user disconnected')
---
+++ src/player.ts
@@ -0,0 +1,5 @@
+export interface Player {
+    name: string;
+    vote: number;
+    position: number;
+}
