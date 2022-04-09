ded710bfbd01c94f96f7b502bc894faa53b54e3f
Filipe de Carvalho Koch <filipe.koch@gmail.com>
2022-04-09 00:19:30 -0300

    add workiong version with frntend

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,50 @@
-import express from 'express';
-import { Server } from 'socket.io';
-import http from 'http';
-import { Player } from './player';
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import { ChangePositionMessage, Player } from "./player";
 
 const PORT = process.env.PORT || 8899;
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: '*'
+  cors: {
+    origin: "*",
+  },
+});
+
+let players: Player[]  = [];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("select place", (changePositionMessageStr: string) => {
+
+    const changePositionMessage: ChangePositionMessage = JSON.parse(changePositionMessageStr);
+
+    const playerIndex = players.findIndex(p => p.uuid === changePositionMessage.player.uuid);
+
+    if (playerIndex >= 0) {
+        players[playerIndex] = {...changePositionMessage.player, position: changePositionMessage.targetPosition};
+    } else {
+        players = [...players, {...changePositionMessage.player, position: changePositionMessage.targetPosition}];
     }
-});
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
-});
+    players = players.sort((a, b) => a.position - b.position);
 
-io.on('connection', (socket) => {
+    io.emit("select place", players)
 
-    console.log('user connected');
-    let players: Player[] = [];
+  });
 
-    socket.on('select place', (player: Player) => {
-        console.log(players);
-        players = [
-            ...players,
-            player
-        ]
-        console.log(players);
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
 
-        socket.emit('select place', players);
-    })
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-    });
 });
 
 httpServer.listen(PORT, () => {
-    console.log(`Server started at ${PORT}`)
+  console.log(`Server started at ${PORT}`);
 });
--- src/player.ts
+++ src/player.ts
@@ -1,5 +1,11 @@
 export interface Player {
     name: string;
     vote: number;
+    uuid: string;
     position: number;
-}
+  }
+
+  export interface ChangePositionMessage {
+    targetPosition: number;
+    player: Player;
+  }
